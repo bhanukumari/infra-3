3001e51803065abd552b394ba1d3c53e7c1c6613
Ubuntu <ubuntu@ip-172-31-29-68.us-east-2.compute.internal>
2024-04-09 08:02:34 +0000

    updated code

---
+++ terraform/php/vpc/variable.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr" {
+    description = "The CIDR block for the VPC."
+    type        = string
+}
+
+variable "vpc" {
+    description = "Name tag of the VPC"
+    type        = string
+}
