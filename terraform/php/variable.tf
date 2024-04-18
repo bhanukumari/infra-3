991f8401ac652ceff402183c572a9761ea6ae25e
bhanukumari <117707378+bhanukumari@users.noreply.github.com>
2024-04-18 15:25:30 +0530

    Update variable.tf

--- terraform/php/variable.tf
+++ terraform/php/variable.tf
@@ -1,5 +1,10 @@
-variable "vpc_cidr1" {
+variable "vpc_cidr" {
   description = "The CIDR block for the VPC."
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name tag of the VPC"
   type        = string
 }
 
