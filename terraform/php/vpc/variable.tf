a4cb5c23dde7f56c8ecf7544a685914ef7239011
bhanukumari <117707378+bhanukumari@users.noreply.github.com>
2024-04-18 15:57:46 +0530

    Update variable.tf

--- terraform/php/vpc/variable.tf
+++ terraform/php/vpc/variable.tf
@@ -1,9 +1,10 @@
 variable "vpc_cidr" {
-  description = "The CIDR block for the VPC."
-  type        = string
+    type = string
+
+  default = "10.0.0.0/16"
 }
 
-variable "vpc_name" {
-  description = "Name tag of the VPC"
-  type        = string
+variable "vpc" {
+    type = string
+    default = "vpc"
 }
